b3ee7f3c6725ed537d6c7924e3fa50ab5cb61257
JCamyre <jwcamry03@gmail.com>
2022-11-04 18:58:26 -0700

    Add basic navigation between pages. Remove extra package.json. Fix npm install issues.

--- bruinmarket/src/App.js
+++ bruinmarket/src/App.js
@@ -1,22 +1,17 @@
-import './App.css';
+import "./App.css";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/pages/Home.jsx";
+import AllPosts from "./components/pages/AllPosts.jsx";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/allposts" element={<AllPosts />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- bruinmarket/src/components/pages/AllPosts.jsx
+++ bruinmarket/src/components/pages/AllPosts.jsx
@@ -1,1 +1,7 @@
-// insert code here
+import React from "react";
+
+function AllPosts() {
+  return <div>AllPosts</div>;
+}
+
+export default AllPosts;
