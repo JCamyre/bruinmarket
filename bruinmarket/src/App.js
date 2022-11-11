0e66b6959ed57c7ff342613b983b058ce19cb7d8
Justin Sheu <justinsheu3341@gmail.com>
2022-11-11 13:29:27 -0800

    added UI for sidebar panel to have clickable categories, now supports open and close panel feature

--- bruinmarket/src/App.js
+++ bruinmarket/src/App.js
@@ -3,15 +3,18 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/pages/Home.jsx";
 import AllPosts from "./components/pages/AllPosts.jsx";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<Home />} />
-        <Route path="/allposts" element={<AllPosts />} />
-      </Routes>
-    </BrowserRouter>
+    <ChakraProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/allposts" element={<AllPosts />} />
+        </Routes>
+      </BrowserRouter>
+    </ChakraProvider>
   );
 }
 
