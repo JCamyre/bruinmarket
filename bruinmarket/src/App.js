1400b5a8095b51b0a52b4f7dce372b10b1ed7be6
adivate2021 <44125685+adivate2021@users.noreply.github.com>
2022-11-12 01:24:28 -0800

    Created a skeleton structure for the profile page

--- bruinmarket/src/App.js
+++ bruinmarket/src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/pages/Home.jsx";
 import AllPosts from "./components/pages/AllPosts.jsx";
+import Profile from "./components/pages/Profile.jsx";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route index element={<Home />} />
         <Route path="/allposts" element={<AllPosts />} />
+        <Route path="/profile" element = {<Profile />} />
       </Routes>
     </BrowserRouter>
   );
