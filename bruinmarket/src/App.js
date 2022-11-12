13bcae2166f18f551f07b586e9083d0b378b028c
JCamyre <jwcamry03@gmail.com>
2022-11-11 21:55:29 -0800

    Add basic route for posts with a URL parameter and a basic Post page

--- bruinmarket/src/App.js
+++ bruinmarket/src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/pages/Home.jsx";
 import AllPosts from "./components/pages/AllPosts.jsx";
+import Post from "./components/pages/Post.jsx";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route index element={<Home />} />
         <Route path="/allposts" element={<AllPosts />} />
+        <Route path="/post/:id" element={<Post />} />
       </Routes>
     </BrowserRouter>
   );
