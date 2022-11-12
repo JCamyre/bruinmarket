15a207871d9b2a4fc9652ec402608528231dbd72
JCamyre <jwcamry03@gmail.com>
2022-11-11 23:42:45 -0800

    Readd Post.jsx progress I lost from rebase fiasco

--- bruinmarket/src/App.js
+++ bruinmarket/src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/pages/Home.jsx";
 import AllPosts from "./components/pages/AllPosts.jsx";
+import Post from "./components/pages/Post.jsx";
 import { ChakraProvider } from "@chakra-ui/react";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route index element={<Home />} />
           <Route path="/allposts" element={<AllPosts />} />
+          <Route path="/post/:id" element={<Post />} />
         </Routes>
       </BrowserRouter>
     </ChakraProvider>
--- bruinmarket/src/components/pages/Post.jsx
+++ bruinmarket/src/components/pages/Post.jsx
@@ -1,39 +1,34 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-<<<<<<< HEAD
-
-function Post() {
-  const { id } = useParams();
-  return <div>Post {id}</div>;
-=======
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from "react-responsive-carousel";
-import { Container, Box } from "chakra-ui";
+import { Container, Box, Center, Img } from "@chakra-ui/react";
 
 function Post() {
   const { id } = useParams();
   return (
-    <div>
-      <h1>Post {id}</h1>
-      <div>
-        <Carousel showArrows={true} infiniteLoop={true}>
+    <Container display="flex" maxW="2xl">
+      <Box w="80%">
+        <Carousel showArrows={true} infiniteLoop={true} showIndicators={false}>
           <div>
-            <img src="https://imgs.search.brave.com/iDWBRAOg5OxWbGv_P4lkcXBZQ6__WvX4XEljLG9FP_A/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9zYmx5/LXdlYi1wcm9kLXNo/YXJlYWJseS5uZXRk/bmEtc3NsLmNvbS93/cC1jb250ZW50L3Vw/bG9hZHMvMjAyMC8w/Ny8xNTIwNTMxOC9z/aWQtYmFsYWNoYW5k/cmFuLV85YS0zTk81/S0pFLXVuc3BsYXNo/LmpwZw" />
+            <Img src="https://imgs.search.brave.com/iDWBRAOg5OxWbGv_P4lkcXBZQ6__WvX4XEljLG9FP_A/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9zYmx5/LXdlYi1wcm9kLXNo/YXJlYWJseS5uZXRk/bmEtc3NsLmNvbS93/cC1jb250ZW50L3Vw/bG9hZHMvMjAyMC8w/Ny8xNTIwNTMxOC9z/aWQtYmFsYWNoYW5k/cmFuLV85YS0zTk81/S0pFLXVuc3BsYXNo/LmpwZw" />
             <p>Panda 1</p>
           </div>
           <div>
-            <img src="https://imgs.search.brave.com/Dghzc_7RTRGKdeVmWN5zRL0tV24jSh8-1XXp9SvSveg/rs:fit:1200:900:1/g:ce/aHR0cHM6Ly9hc2tn/YW1lci5jb20vd3At/Y29udGVudC91cGxv/YWRzLzIwMjEvMDQv/QnJlZWQtUGFuZGFz/LmpwZw" />
+            <Img src="https://imgs.search.brave.com/Dghzc_7RTRGKdeVmWN5zRL0tV24jSh8-1XXp9SvSveg/rs:fit:1200:900:1/g:ce/aHR0cHM6Ly9hc2tn/YW1lci5jb20vd3At/Y29udGVudC91cGxv/YWRzLzIwMjEvMDQv/QnJlZWQtUGFuZGFz/LmpwZw" />
             <p>Minecraft Panda</p>
           </div>
           <div>
-            <img src="https://imgs.search.brave.com/Us-94VJX9bkGOxLCqzLhUYllfI9NMAs0IUj9dbpd_TM/rs:fit:1158:693:1/g:ce/aHR0cHM6Ly9zdGF0/aWMucGxhbmV0bWlu/ZWNyYWZ0LmNvbS9m/aWxlcy9yZXNvdXJj/ZV9tZWRpYS9zY3Jl/ZW5zaG90LzE5MjMv/My0xNTU5NzkwMzg2/X2xyZy5wbmc" />
+            <Img src="https://imgs.search.brave.com/Us-94VJX9bkGOxLCqzLhUYllfI9NMAs0IUj9dbpd_TM/rs:fit:1158:693:1/g:ce/aHR0cHM6Ly9zdGF0/aWMucGxhbmV0bWlu/ZWNyYWZ0LmNvbS9m/aWxlcy9yZXNvdXJj/ZV9tZWRpYS9zY3Jl/ZW5zaG90LzE5MjMv/My0xNTU5NzkwMzg2/X2xyZy5wbmc" />
             <p>Cursed Panda</p>
           </div>
         </Carousel>
-      </div>
-    </div>
+      </Box>
+      <Box w="20%">
+        <Center>Post {id}</Center>
+      </Box>
+    </Container>
   );
->>>>>>> 7855b5d421288bd0c568ae22d8cacfe865727294
 }
 
 export default Post;
