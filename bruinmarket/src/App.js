6a59de2545ef91552d2b54c7173c585d7d83286d
JCamyre <jwcamry03@gmail.com>
2022-11-11 23:43:55 -0800

    Fix

--- bruinmarket/src/App.js
+++ bruinmarket/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/pages/Home.jsx";
 import AllPosts from "./components/pages/AllPosts.jsx";
 import Post from "./components/pages/Post.jsx";
+<<<<<<< HEAD
 import { ChakraProvider } from "@chakra-ui/react";
 
 function App() {
@@ -17,6 +18,18 @@ function App() {
         </Routes>
       </BrowserRouter>
     </ChakraProvider>
+=======
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/allposts" element={<AllPosts />} />
+        <Route path="/post/:id" element={<Post />} />
+      </Routes>
+    </BrowserRouter>
+>>>>>>> Add basic route for posts with a URL parameter and a basic Post page
   );
 }
 
