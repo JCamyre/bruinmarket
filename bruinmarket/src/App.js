254f007893d965a81b9e0dee9c8c5e224e1f040b
Justin Sheu <justinsheu3341@gmail.com>
2022-11-19 02:37:17 -0800

    added firebase setup and registration function for email/password registration that interfaces with google firebase/firestore and that checks for registration errors

--- bruinmarket/src/App.js
+++ bruinmarket/src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/pages/Home.jsx";
 import AllPosts from "./components/pages/AllPosts.jsx";
+import TempRegister from "./components/pages/TempRegister.jsx"
 import { ChakraProvider } from "@chakra-ui/react";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route index element={<Home />} />
           <Route path="/allposts" element={<AllPosts />} />
+          <Route path="/testregister" element={<TempRegister />} />
         </Routes>
       </BrowserRouter>
     </ChakraProvider>
