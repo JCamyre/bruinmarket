116f9411a730216c0ec446cbcf56831fe2c5d2ce
JCamyre <jwcamry03@gmail.com>
2022-11-18 22:16:28 -0800

    Add React state to track and submit password and email

--- bruinmarket/src/components/pages/Login.jsx
+++ bruinmarket/src/components/pages/Login.jsx
@@ -2,21 +2,35 @@ import React, { useState } from "react";
 import { Button, Container, Input, Stack, Link } from "@chakra-ui/react";
 
 function Login() {
-  const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function Submit(e) {
+    e.preventDefault();
+    console.log(`Email: ${email}, Password: ${password}`);
+    // need to redirect to / page
+  }
 
   return (
     <Container maxW="container.md" pt="40">
       <Container maxW="md">
-        <Stack spacing="8">
-          <Input
-            placeholder="username"
-            onChange={(e) => console.log(e.currentTarget.value)}
-          />
-          <Input placeholder="password" />
-          <Button onClick={(e) => console.log("Yo")}>Sign in</Button>
-          <Link href="#">Don't have an account, register here!</Link>
-        </Stack>
+        <form onSubmit={Submit} method="POST">
+          <Stack spacing="8">
+            <Input
+              placeholder="username"
+              onChange={(e) => setEmail(e.currentTarget.value)}
+            />
+            <Input
+              placeholder="password"
+              onChange={(e) => setPassword(e.currentTarget.value)}
+            />
+            <Link href="/">
+              <Button type="submit">Sign in</Button>
+            </Link>
+
+            <Link href="/">Don't have an account, register here!</Link>
+          </Stack>
+        </form>
       </Container>
     </Container>
   );
