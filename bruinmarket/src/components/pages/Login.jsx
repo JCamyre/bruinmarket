df856df4547bef5f324593ac93b3eb611eb4e86a
JCamyre <jwcamry03@gmail.com>
2022-11-22 22:48:51 -0800

    Add redirect to home page after successfully logging in

--- bruinmarket/src/components/pages/Login.jsx
+++ bruinmarket/src/components/pages/Login.jsx
@@ -1,14 +1,18 @@
 import React, { useState } from "react";
 import { Button, Container, Input, Stack, Link } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const navigate = useNavigate();
+
   function Submit(e) {
     e.preventDefault();
     console.log(`Email: ${email}, Password: ${password}`);
     // need to redirect to / page
+    navigate("/");
   }
 
   return (
