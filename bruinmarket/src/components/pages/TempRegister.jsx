3faaee7e9995d4f310f94ef8b02b31f39a2a47a7
Justin Sheu <justinsheu3341@gmail.com>
2022-11-25 15:51:02 -0700

    added logout flow, registering now logs user in, user data is now passed in after login

--- bruinmarket/src/components/pages/TempRegister.jsx
+++ bruinmarket/src/components/pages/TempRegister.jsx
@@ -1,6 +1,8 @@
 import React from "react"
 import {Input, Button, Text} from "@chakra-ui/react"
 import register from "../../register.js"
+import login from "../../login"
+import { useNavigate } from "react-router-dom"; 
 
 
 
@@ -9,8 +11,33 @@ function Registration() {
     const [email, setEmail] = React.useState()
     const [submittedEmail, setSubmittedEmail] = React.useState()
     const [password, setPassword] = React.useState()
-    const [accountAlreadyExists, setExists] = React.useState(true)
     const [status, setStatus] = React.useState(5)
+
+    const navigate = useNavigate();
+
+
+    async function Submit(e) {
+        e.preventDefault();
+        console.log(`Email: ${email}, Password: ${password}`);
+    
+        // insert call to database to verify email and password
+        const registerResult = await register(username, email, password, setStatus)
+        let loginResult = null
+        if (!registerResult) {
+            loginResult = await login(email, password, setStatus);
+        }
+        setSubmittedEmail(email);
+        // if user authenticated, get whatever information we need related to their information
+    
+        // reroute to home page if authenticated
+        // otherwise don't change page and show some error
+        if (!loginResult) {
+          navigate("/");
+        } else {
+          setStatus(loginResult)
+        }
+      }
+
     const getStatusMessage = (status) =>
     {
         console.log(status)
@@ -35,7 +62,7 @@ function Registration() {
             <Input placeholder="email" onChange={e=>setEmail(e.currentTarget.value)}></Input>
             <Input placeholder="password" onChange={e=>setPassword(e.currentTarget.value)}></Input>
 
-            <Button onClick={() => {register(username, email, password, setStatus); setSubmittedEmail(email)}}>Register</Button>
+            <Button onClick={Submit}>Register</Button>
             <Text color="red"> {getStatusMessage(status)} </Text>
 
         </div>
