be7402798d07d28c4192243b4a903f547feb02c2
JCamyre <jwcamry03@gmail.com>
2022-11-25 00:17:10 -0800

    Fix some styling and add file upload support. Add some comments to give hints for Aryan to add backend calls

--- bruinmarket/src/components/pages/CreatePost.jsx
+++ bruinmarket/src/components/pages/CreatePost.jsx
@@ -10,16 +10,44 @@ import {
   Textarea,
 } from "@chakra-ui/react";
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function CreatePost() {
   const [title, setTitle] = useState("");
   const [summary, setSummary] = useState("");
   const [category, setCategory] = useState("");
 
+  const navigate = useNavigate();
+
   function Submit(e) {
     e.preventDefault();
     // need to redirect to / page
     console.log(`Title: ${title}, Summary: ${summary}, Category: ${category}`);
+
+    // Maybe we'll add a check to check for duplicate posts, but for now, just push to the Post table
+    // WE NEED TO ADD A DATABASE CALL THAT RETURNS THE ID OF THE CURRENT USER LOGGED IN
+
+    // database.post(user_id, title, summary, category)
+
+    // pictures we will figure out.
+
+    // If everything goes well, just redirect
+    if (true) {
+      return navigate("/");
+    } else {
+      console.log("You fricked up");
+    }
+  }
+
+  // img should be a file
+  function uploadPicture(imgs) {
+    const imgsArr = [...imgs];
+
+    imgsArr.forEach((img, i) => {
+      // FOR EACH IMAGE, UPLOAD THE FILE TO FIREBASE, WITH THE OUR WACKY NAMING SYSTEM
+      // POSTID + _ + PICTURE NUMBER (use the i variable for PICTURE NUMBER)
+      console.log(img, i);
+    });
   }
 
   return (
@@ -36,7 +64,7 @@ function CreatePost() {
             onChange={(e) => setSummary(e.currentTarget.value)}
           />
           <FormControl>
-            <FormLabel>Country</FormLabel>
+            <FormLabel>Category</FormLabel>
             <Select
               placeholder="Select category"
               onChange={(e) => setCategory(e.currentTarget.value)}
@@ -45,9 +73,15 @@ function CreatePost() {
               <option>Books</option>
             </Select>
           </FormControl>
-          <Link href="/">
-            <Button type="submit">Post!</Button>
-          </Link>
+          <Input
+            type="file"
+            placeholder="filename"
+            multiple
+            onChange={(e) => uploadPicture(e.currentTarget.files)}
+          />
+          <Button type="submit" maxW="sm">
+            Post!
+          </Button>
         </Stack>
       </form>
     </Container>
