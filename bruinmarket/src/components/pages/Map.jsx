627ef17d6dcaa7a9868ea17a452b359e025b72e5
adivate2021 <44125685+adivate2021@users.noreply.github.com>
2022-11-30 02:38:19 -0800

    Made the map more zoomed in and added custom marker, made the map smaller so the right size when it will be implemented on the post page

--- bruinmarket/src/components/pages/Map.jsx
+++ bruinmarket/src/components/pages/Map.jsx
@@ -1,6 +1,13 @@
 import React, {useState, useEffect} from "react";
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { Icon } from 'leaflet'
+import {Container, VStack} from "@chakra-ui/react"
 import '../App.css';
+import marker from '../marker.png';
+const myIcon = new Icon({
+    iconUrl: marker,
+    iconSize: [22,32]
+   })
 
 // function componentDidMount() {
 // navigator.geolocation.getCurrentPosition(function(position) {
@@ -27,18 +34,23 @@ function App() {
     })
   return (
       <div>
+        <Container maxW="container.md" pt="40">
+        <VStack>
         <link
             rel="stylesheet"
             href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"
             integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
             crossorigin=""
         />
-        <MapContainer center={[latitude, longitude]} zoom={12}scrollWheelZoom={false}>
+        <MapContainer center={[34.0725, -118.4522]} zoom={16}scrollWheelZoom={false}>
         <TileLayer
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         />
+        <Marker position={[34.0725, -118.4522]} icon={myIcon}/>
         </MapContainer>
+        </VStack>
+        </Container>
       </div>
   );
 }
