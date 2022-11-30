52fe951f83530f9cb1e75f0b1065dbd62ecf98ec
JCamyre <jwcamry03@gmail.com>
2022-11-29 19:31:22 -0800

    Update Navbar styles.

--- bruinmarket/src/App.js
+++ bruinmarket/src/App.js
@@ -3,19 +3,19 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/pages/Home.jsx";
 import AllPosts from "./components/pages/AllPosts.jsx";
-import TempRegister from "./components/pages/TempRegister.jsx"
+import TempRegister from "./components/pages/TempRegister.jsx";
 import { ChakraProvider } from "@chakra-ui/react";
 import Navbar from "./components/Navbar";
 import Login from "./components/pages/Login.jsx";
-import ProtectedRoute from "./utilities/ProtectedRoute"
-import { auth, firestore, database } from "./firebase"
+import ProtectedRoute from "./utilities/ProtectedRoute";
+import { auth, firestore, database } from "./firebase";
 
-const AuthContext = React.createContext(null)
+const AuthContext = React.createContext(null);
 
 function App() {
   const [loggedIn, setLoggedIn] = React.useState(-1); //initialize to -1 to account for period of time when login state is indeterminate
-  const [userUID, setUserUID] = React.useState(null)
-  const [userData, setData] = React.useState(null)
+  const [userUID, setUserUID] = React.useState(null);
+  const [userData, setData] = React.useState(null);
   React.useEffect(() => {
     auth.onAuthStateChanged((user) => {
       if (user) {
@@ -23,7 +23,7 @@ function App() {
         setUserUID(user.uid);
         // console.log(userData.username)
       } else {
-        console.log ("successfully logged out")
+        console.log("successfully logged out");
         setLoggedIn(0);
         setUserUID(null);
         // console.log(userData.username)
@@ -31,40 +31,56 @@ function App() {
     });
   }, []); //useEffect prevents onAuthStateChanged listener from being set multiple times
   React.useEffect(() => {
-    async function getUserData (uid) {
-      const q = firestore.query(firestore.collection(database, "users"), firestore.where("uid", "==", uid))
-      const docs = await firestore.getDocs(q)
-      let data = null
+    async function getUserData(uid) {
+      const q = firestore.query(
+        firestore.collection(database, "users"),
+        firestore.where("uid", "==", uid)
+      );
+      const docs = await firestore.getDocs(q);
+      let data = null;
       docs.forEach((doc) => {
         // console.log(doc.data().username)
-        data = doc.data()
-      })
-      console.log(data)
-      setData(data)
+        data = doc.data();
+      });
+      console.log(data);
+      setData(data);
     }
     if (userUID) {
-      getUserData(userUID)
+      getUserData(userUID);
     }
-  }, [userUID])
+  }, [userUID]);
   return (
     <ChakraProvider>
-
       <AuthContext.Provider value={userData}>
-            <Navbar />
+        <Navbar />
         <BrowserRouter>
           <Routes>
-            {loggedIn === -1 ? null :
+            {loggedIn === -1 ? null : (
               <>
-                <Route element={<ProtectedRoute allowed={loggedIn===1} alternateRoute="/login" />} >
+                <Route
+                  element={
+                    <ProtectedRoute
+                      allowed={loggedIn === 1}
+                      alternateRoute="/login"
+                    />
+                  }
+                >
                   <Route path="/" element={<Home />} />
                   <Route path="/allposts" element={<AllPosts />} />
-                </Route>              
-                <Route element={<ProtectedRoute allowed={loggedIn===0} alternateRoute="/" />} >
+                </Route>
+                <Route
+                  element={
+                    <ProtectedRoute
+                      allowed={loggedIn === 0}
+                      alternateRoute="/"
+                    />
+                  }
+                >
                   <Route path="/testregister" element={<TempRegister />} />
                   <Route path="/login" element={<Login />} />
                 </Route>
-              </>    
-            }          
+              </>
+            )}
           </Routes>
         </BrowserRouter>
       </AuthContext.Provider>
@@ -72,5 +88,5 @@ function App() {
   );
 }
 
-export {AuthContext};
+export { AuthContext };
 export default App;
